590f23c64b7a1736a686542172b4ad5c94cf28e5
vall9133 <manojvallabhaneni16@gmail.com>
2025-04-17 12:43:35 -0400

    Updates

---
+++ precache-manifest.d2dfee8744645ab77faf695f979ee7b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75fbdb40cb33472ad4f4fbf549ff06ae",
+    "url": "/manoj-portfolio/index.html"
+  },
+  {
+    "revision": "f8d55810bf45fb4f7afa",
+    "url": "/manoj-portfolio/static/css/main.aae29173.chunk.css"
+  },
+  {
+    "revision": "8a7c83afd89b2c30a09e",
+    "url": "/manoj-portfolio/static/js/2.24f3bc64.chunk.js"
+  },
+  {
+    "revision": "f8d55810bf45fb4f7afa",
+    "url": "/manoj-portfolio/static/js/main.6627c276.chunk.js"
+  },
+  {
+    "revision": "d26471471a64e4c1f538",
+    "url": "/manoj-portfolio/static/js/runtime~main.5d8192de.js"
+  }
+]);
